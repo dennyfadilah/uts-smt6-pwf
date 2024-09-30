5313ff6f231528f1c67df407cc425661738f0e33
dennyfadilah <dennyfadilah99@gmail.com>
2024-09-30 21:57:25 +0700

    first commit

---
+++ app/Views/app.php
@@ -0,0 +1,9 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+<title>App</title>
+
+<div class="container p-2">
+    <div class="card px-2 bg-warning">Views/app</div>
+</div>
+<?= $this->endSection() ?>
---
+++ app/Views/layouts/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
+</head>
+
+<body>
+    <?= $this->renderSection('content') ?>
+    <script src="<?= base_url('vendor/bootrap/js/bootstrap.bundle.js') ?>"></script>
+</body>
+
+</html>
