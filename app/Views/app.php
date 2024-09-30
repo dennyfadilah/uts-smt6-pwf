d6ae9328a9d1583758d57410182a861bb0ea7e13
dennyfadilah <dennyfadilah99@gmail.com>
2024-09-30 22:17:06 +0700

    add bootstrap-icons

--- app/Views/app.php
+++ app/Views/app.php
@@ -3,7 +3,10 @@
 <?= $this->section('content') ?>
 <title>App</title>
 
-<div class="container p-2">
-    <div class="card px-2 bg-warning">Views/app</div>
+<div class="container p-5">
+    <div class="card p-2 bg-warning text-center">
+        <i class="bi bi-exclamation-triangle fs-1"></i>
+        <p class="lead my-0">Views/app</p>
+    </div>
 </div>
 <?= $this->endSection() ?>
--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= base_url('vendor/bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('vendor/bootstrap-icons/font/bootstrap-icons.min.css') ?>">
 </head>
 
 <body>
